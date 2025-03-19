87744bafb11244fd166348f7f315d2c647f693b7
TIFFANY WONG <twong402@my.bcit.ca>
2025-03-18 19:48:20 -0700

    Complete parts 1 and 2 in demo10

--- scripts/eachHike.js
+++ scripts/eachHike.js
@@ -20,3 +20,10 @@ function displayHikeInfo() {
         });
 }
 displayHikeInfo();
+
+function saveHikeDocumentIDAndRedirect() {
+    let params = new URL(window.location.href) //get the url from the search bar
+    let ID = params.searchParams.get("docID");
+    localStorage.setItem('hikeDocID', ID);
+    window.location.href = 'review.html';
+}
